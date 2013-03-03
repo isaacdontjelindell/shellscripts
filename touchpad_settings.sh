62cb5d28c52b1027984f3ab330094cecb9d46cbb
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-03-03 15:11:24 -0600

    added touchpad script

---
+++ touchpad_settings.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# allow 2 finger && edge scrolling
+sleep 10s; # if it's still not working on startup try increasing the delay
+synclient VertEdgeScroll=1;
+synclient HorizEdgeScroll=1;
+synclient VertTwoFingerScroll=1;
+synclient HorizTwoFingerScroll=1;
+ 
+# enable 2 finger-tap to middle click
+xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Tap Action" 8 9 0 0 1 2 3
