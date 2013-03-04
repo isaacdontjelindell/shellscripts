2c2a4798075d52595e23c88ddf32530127bb5825
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-03-03 18:39:00 -0600

    added comment indicating how to run the script after resume

--- touchpad_settings.sh
+++ touchpad_settings.sh
@@ -9,3 +9,6 @@ synclient HorizTwoFingerScroll=1;
  
 # enable 2 finger-tap to middle click
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Tap Action" 8 9 0 0 1 2 3
+
+# To run this script after suspend/resume:
+# gsettings set org.gnome.settings-daemon.peripherals.input-devices hotplug-command "/home/isaac/trackpad_settings"
