4ed3ccc250f1f69275a62e5f396697a15d28bb36
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-11-14 13:08:35 -0600

    update touchpad_settings

--- touchpad_settings.sh
+++ touchpad_settings.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # allow 2 finger && edge scrolling
-sleep 10s; # if it's still not working on startup try increasing the delay
 synclient VertEdgeScroll=1;
 synclient HorizEdgeScroll=1;
 synclient VertTwoFingerScroll=1;
@@ -12,3 +11,6 @@ xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Tap Action" 8 9 0 0 1 2 
 
 # To run this script after suspend/resume:
 # gsettings set org.gnome.settings-daemon.peripherals.input-devices hotplug-command "/home/isaac/github/shellscripts/touchpad_settings.sh"
+
+# To stop Ubuntu from overriding these settings on boot:
+# gsettings set org.gnome.settings-daemon.plugins.mouse active false
