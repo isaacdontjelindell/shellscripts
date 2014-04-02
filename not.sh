c5dc840c291613146d3e97aee52258cd539e919a
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2014-04-02 10:59:00 -0500

    add some docs to not.sh

--- not.sh
+++ not.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+# This command is designed to present an Ubuntu GUI notification (libnotify).
+# I use it to notify me when job in a terminal that's not visible finishes.
+# For example: "$ sudo apt-get update ; not.sh"
+# The notification will appear after the apt-get completes.
+
+# Usage: You will want to make sure that not.sh is on your PATH.
+# I've symlinked not.sh to n (saves some typing).
+
+
 notify-send "Terminal Job Finished" "______________________"
